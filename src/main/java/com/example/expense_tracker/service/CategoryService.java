c845951d6f9d79502d8332ab4b2d176f74049c3f
Tarushi <tarushiarora25@gmail.com>
2025-11-01 17:43:51 +0530

    updated Category Service

--- src/main/java/com/example/expense_tracker/service/CategoryService.java
+++ src/main/java/com/example/expense_tracker/service/CategoryService.java
@@ -5,6 +5,9 @@ import com.example.expense_tracker.repository.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
 public class CategoryService {
     private final CategoryRepository categoryRepository;
@@ -18,7 +21,29 @@ public class CategoryService {
 
     // CREATE
     public Category createCategory(Category category){
-        return CategoryRepository.save(category);
+        return categoryRepository.save(category);
     }
 
+    // READ
+    public List<Category> getAllCategories(){
+        return categoryRepository.findAll();
+    }
+
+    // read(by id)
+    public Optional<Category> getIdByCategory(Long id){
+        return categoryRepository.findById(id);
+    }
+
+    // UPDATE
+
+    // delete
+    public boolean deleteCategory(Long id){
+        if(categoryRepository.existsById(id)){
+            // will check later if my transaction is having this category
+            // before deleting
+            categoryRepository.deleteById(id);
+            return true;
+        }
+        return false;
+    }
 }
