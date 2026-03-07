e9f80b9df80d8a876127ba1a6498d2e0f2839758
Tarushi <tarushiarora25@gmail.com>
2026-03-07 11:42:55 +0530

    Trusting live frontend URL

--- src/main/java/com/example/expense_tracker/config/SecurityConfig.java
+++ src/main/java/com/example/expense_tracker/config/SecurityConfig.java
@@ -77,7 +77,8 @@ public class SecurityConfig {
     @Bean
     public org.springframework.web.cors.CorsConfigurationSource corsConfigurationSource() {
         org.springframework.web.cors.CorsConfiguration configuration = new org.springframework.web.cors.CorsConfiguration();
-        configuration.setAllowedOrigins(java.util.Arrays.asList("http://localhost:4200")); // Your laptop's address
+        configuration.setAllowedOrigins(java.util.Arrays.asList("http://localhost:4200",
+                "https://expense-tracker-ui-fbuu.onrender.com"));
         configuration.setAllowedMethods(java.util.Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(java.util.Arrays.asList("Authorization", "Content-Type"));
         org.springframework.web.cors.UrlBasedCorsConfigurationSource source = new org.springframework.web.cors.UrlBasedCorsConfigurationSource();
